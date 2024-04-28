e2a055c48c99d2ecc55601120fa1fd6db5429a69
alvaro plaza <taponplaza@gmail.com>
2024-04-28 14:03:27 +0200

    pruebas actualizadas

--- input.c
+++ input.c
@@ -1,6 +1,14 @@
+int main(int argc, int argv){
+    int b;
+}
+int main2(int argd, int arge){
+    int f;
+}
+
 int myNumbers[4][3] = {
     {1, 2, 3},
     {4, 5, 6},
     {7, 8, 9},
     {10, 11, 12}
 };
+float b = 3.14;
